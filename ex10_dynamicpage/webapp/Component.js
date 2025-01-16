250d240f8207554e68e9169756f71dc369de1556
wkddns1 <wkddns1@naver.com>
2025-01-16 04:04:46 +0000

    add ex10 code

---
+++ ex10_dynamicpage/webapp/Component.js
@@ -0,0 +1,52 @@
+/**
+ * eslint-disable @sap/ui5-jsdocs/no-jsdoc
+ */
+
+sap.ui.define([
+        "sap/ui/core/UIComponent",
+        "sap/ui/Device",
+        "student00/sap/training/dynamicpage/model/models",
+        "student00/sap/training/dynamicpage/controller/ListSelector"
+
+    ],
+    function (UIComponent, Device, models, ListSelector) {
+        "use strict";
+
+        return UIComponent.extend("student00.sap.training.dynamicpage.Component", {
+            metadata: {
+                manifest: "json"
+            },
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // enable routing
+                this.getRouter().initialize();
+
+                // set the device model
+                this.setModel(models.createDeviceModel(), "device");
+
+                this.oListSelector = new ListSelector();
+
+            },
+            getContentDensityClass: function() {
+                if(!this._sContentDensityClass) {
+                    if(!Device.support.touch) {
+                        this._sContentDensityClass= "sapUiSizeCompact";                                    
+                    } else {
+                        this._sContentDensityClass= "sapUiSizeCozy";
+                    }
+                }
+                return this._sContentDensityClass;
+            }   
+
+
+        });
+    }
+);
