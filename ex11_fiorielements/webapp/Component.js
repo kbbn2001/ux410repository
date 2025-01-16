a4f7f9f71d7f29a77ad3022c49f0388db57a6abc
wkddns1 <wkddns1@naver.com>
2025-01-16 06:31:52 +0000

    add ex11 code

---
+++ ex11_fiorielements/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("student00.sap.training.fiorielements.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
